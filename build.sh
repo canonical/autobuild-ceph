25a744d565d087f94b21bbf3d74bd0a5abb9cdf0
Luciano Lo Giudice <luciano.logiudice@canonical.com>
2026-04-06 20:22:58 -0300

    Build Ceph in Ubuntu Resolute
    Signed-off-by: Luciano Lo Giudice <luciano.logiudice@canonical.com>

---
+++ build.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+set -eux
+
+sudo apt update
+sudo apt install devscripts git-buildpackage equivs python3-venv default-jdk javahelper dh-python
+
+# check out upstream repo and generate tarball
+git clone https://github.com/ceph/ceph
+cd ceph && ./make-dist && cd ..
+
+mv ceph/ceph*.bz2 ceph-tarball.tar.bz2
+rmdir ceph
+
+# check out launchpad repo
+git clone git://git.launchpad.net/~lmlogiudice/ubuntu/+source/ceph
+cd ceph
+
+git remote add source git://git.launchpad.net/ubuntu/+source/ceph
+git checkout upstream && git clone pristine-tar
+git fetch source
+git checkout origin/ubuntu/resolute
+git checkout -b ubuntu/resolute || true
+
+# incorporate tarball into a tag
+gbp import-orig --no-interactive --merge-mode=replace ../ceph-tarball.tar.bz2 -u 20.2.0
+rm *.buildinfo || true
+git checkout upstream/20.2.0
+git checkout -b build
+git checkout ubuntu/latest -- debian
+git commit -m "add debian directory"
+
+sudo sed -i 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources
+sudo mk-build-deps -i -t "apt-get -o Debug::pkgProblemResolver=1 -y --no-install-recommends" debian/control
+debuild --no-lintian -us -uc -d   # build the package
